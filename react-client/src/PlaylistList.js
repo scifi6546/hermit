008269d07b2ab63e49228207a0f4810598111926
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 15:19:39 -0900

    added selecttion

--- react-client/src/PlaylistList.js
+++ react-client/src/PlaylistList.js
@@ -1,40 +1,77 @@
 import React from "react";
 import _ from "lodash";
-import {Button,Icon, Container} from "semantic-ui-react";
+import { Button, Image, Container, Segment } from "semantic-ui-react";
 import Axios from "axios";
-const state={
-    url:"",
-    playlistList:[],
-    videoList:[]
+const state = {
+    url: "",
+    playlistList: [],
+    videoList: [],
+    edit_playlist: [],
 }
-class PlaylistList extends React.Component{
-    constructor(props){
+class PlaylistList extends React.Component {
+    constructor(props) {
         super(props)
-        this.state=_.cloneDeep(state);
-        this.state.url=props.url;
+        this.state = _.cloneDeep(state);
+        this.state.url = props.url;
 
-        this.getPlaylists=this.getPlaylists.bind(this);
-        this.getVideos=this.getVideos.bind(this);
+        this.getPlaylists = this.getPlaylists.bind(this);
+        this.getVideos = this.getVideos.bind(this);
+        this.makePlaylist = this.makePlaylist.bind(this);
+        this.selectVideo = this.selectVideo.bind(this);
 
         this.getVideos();
         this.getPlaylists();
     }
-    async getPlaylists(){
-        let res = await Axios.get(this.state.url+"/api/get_playlist_all");
+    async getPlaylists() {
+        let res = await Axios.get(this.state.url + "/api/get_playlist_all");
     }
-    async getVideos(){
-        let res = await Axios.get(this.state.url+"/api/videos");
-        console.log(res);
+    async getVideos() {
+        let res = await Axios.get(this.state.url + "/api/videos");
+        console.log(res.data.Ok);
         this.setState({
-            videoList:res.data
+            videoList: res.data.Ok
         })
     }
-
-    render(){
+    makePlaylist() {
+        console.log("Created Playlist")
+        this.setState({
+            edit_playlist: ["foo"]
+        })
+    }
+    selectVideo(event, target) {
+        console.log(event)
+        let name = target.id;
+        for (let i = 0; i < this.state.videoList.length; i++) {
+            if (this.state.videoList[i].name === name) {
+                if (this.state.videoList[i].color === "yellow") {
+                    this.state.videoList[i].color = null;
+                } else {
+                    this.state.videoList[i].color = "yellow"
+                }
+                break;
+            }
+        }
+        this.setState({});
+    }
+    render() {
 
         return (
             <Container>
-                <Button>Create New Playlist</Button>
+                <Button onClick={this.makePlaylist}>Create New Playlist</Button>
+                {this.state.edit_playlist.map((foo) =>
+                    <Container>
+                        {this.state.videoList.map((vid) =>
+                            <Segment key={vid.name} inverted color={vid.color}>
+
+                                <Image src={vid.thumbnail_url} />
+                                <Container color="yellow">
+                                    <Button onClick={this.selectVideo} content='Add To Playlist' icon='add' labelPosition='right' color="red" id={vid.name} />
+                                </Container>
+
+                            </Segment>
+                        )}
+                    </Container>
+                )}
             </Container>
         )
     }
