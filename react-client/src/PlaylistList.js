7c5b6e9c922d1c4671abdf793fb76d428461e112
Nick <scifi6546@protonmail.com>
2020-01-21 15:53:24 -0900

    added playlist_add diolog

--- react-client/src/PlaylistList.js
+++ react-client/src/PlaylistList.js
@@ -7,6 +7,7 @@ const state = {
     playlistList: [],
     videoList: [],
     edit_playlist: [],
+    edit_playlist_name:""
 }
 class PlaylistList extends React.Component {
     constructor(props) {
@@ -19,12 +20,21 @@ class PlaylistList extends React.Component {
         this.makePlaylist = this.makePlaylist.bind(this);
         this.selectVideo = this.selectVideo.bind(this);
         this.sumbitPlaylist=this.sumbitPlaylist.bind(this);
+        this.edit_playlist_name=this.edit_playlist_name.bind(this);
 
         this.getVideos();
+        
+    }
+    componentDidMount(){
         this.getPlaylists();
     }
     async getPlaylists() {
         let res = await Axios.get(this.state.url + "/api/get_playlist_all");
+        console.log("geting playlists")
+        console.log(res.data);
+        this.setState({
+            playlistList:res.data
+        })
     }
     async getVideos() {
         let res = await Axios.get(this.state.url + "/api/videos");
@@ -37,6 +47,11 @@ class PlaylistList extends React.Component {
         console.log("Created Playlist")
         this.setState({
             edit_playlist: ["foo"]
+        })
+    }
+    edit_playlist_name(event,target){
+        this.setState({
+            edit_playlist_name:event.target.value,
         })
     }
     selectVideo(event, target) {
@@ -54,11 +69,15 @@ class PlaylistList extends React.Component {
         }
         this.setState({});
     }
-    sumbitPlaylist(){
+    sumbitPlaylist(input){
+        console.log(input.target);
         let videos_in_playlist=[]
         for (let i = 0; i < this.state.videoList.length; i++) {
-
+            if (this.state.videoList[i].color === "yellow")
+                videos_in_playlist.push(this.state.videoList[i].path)
         }
+        let data = {name:this.state.edit_playlist_name,videos:videos_in_playlist};
+        Axios.post(this.state.url+"/api/add_playlist",data)
     }
     render() {
 
@@ -70,12 +89,12 @@ class PlaylistList extends React.Component {
                         <Form onSubmit={this.sumbitPlaylist}>
                             <Form.Field>
                                 <label>Playlist Name</label>
-                                <input placeholder="playlist name"></input>
+                                <input placeholder="playlist name" onChange={this.edit_playlist_name}></input>
                             </Form.Field>
                             <Button type='submit'>Submit</Button>
                         </Form>
                         {this.state.videoList.map((vid) =>
-                            <Segment key={vid.name} inverted color={vid.color}>
+                            <Segment key={vid.name} inverted={vid.color} color={vid.color}>
 
                                 <Image src={vid.thumbnail_url} />
                                 <Container color="yellow">
@@ -84,8 +103,16 @@ class PlaylistList extends React.Component {
 
                             </Segment>
                         )}
+                        
                     </Container>
                 )}
+                {this.state.playlistList.map((play)=>
+                            <Segment key={play.name}>
+                                
+                                <Image src={play.videos[0].thumbnail_url} />
+                                {play.name}
+                            </Segment>
+                        )}
             </Container>
         )
     }
