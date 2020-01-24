11de30e8389a1c918d3800f61897bb58b08ce5d5
Nick <scifi6546@protonmail.com>
2020-01-23 17:41:33 -0900

    added

--- react-client/src/PlaylistPlay.js
+++ react-client/src/PlaylistPlay.js
@@ -1,32 +1,46 @@
 import React from "react";
 import _ from "lodash";
-import { Button, Grid, Icon, Image, Menu, Container } from "semantic-ui-react";
+import { Button, Grid, Icon, Image, Menu, Container, Header } from "semantic-ui-react";
 class Playlist extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { playlist: _.cloneDeep(props.playlist) }
+        this.state = { playlist: _.cloneDeep(props.playlist),
+        playing_video:""}
+    }
+    componentDidMount(){
+        this.setState({
+            playing_video:this.state.playlist.videos[0].name,
+        })
     }
     render() {
 
         return (
             <Container>
-                <Grid>
-                    <Grid.Column width={10}>
-                        <Button icon onClick={this.state.quitVideo}>
-                            <Icon name="close" />
-                        </Button>
-                        <video controls>
-                            <source src={this.state.playlist.videos[0].url} />
-                        </video>
-                    </Grid.Column>
-                    <Grid.Column width={1}>
-                        <Menu vertical>
-                            {this.state.playlist.videos.map((video) =>
-                                <Image src={video.thumbnail_url} />
-                            )}
-                        </Menu>
-                    </Grid.Column>
-                </Grid>
+                <Button icon onClick={this.state.quitVideo}>
+                    <Icon name="close" />
+                </Button>
+                <video controls>
+                    <source src={this.state.playlist.videos[0].url} />
+                </video>
+
+                <Menu style={{ "overflowX": "scroll", "height": "100%" }}>
+
+                    {this.state.playlist.videos.map((video) =>
+                        <Menu.Item active={this.state.playing_video===video.name}>
+                            <div style={{ "display": "flex", "flexDirection": "column", "padding": ".00cm", "width": "100%",}}>
+                                <div>
+                                    <Image src={video.thumbnail_url} style={{"height":"1cm"}}/>
+                                </div>
+                                <div>
+                                    {video.name}
+                                </div>
+                            </div>
+
+                        </Menu.Item>
+                    )}
+
+
+                </Menu>
             </Container>
         )
     }
