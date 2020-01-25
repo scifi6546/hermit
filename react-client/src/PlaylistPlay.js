143f8d14b9797d8d929999dd1901dfe927e078a7
Nick <scifi6546@protonmail.com>
2020-01-24 15:55:28 -0900

    MVP COmplete

--- react-client/src/PlaylistPlay.js
+++ react-client/src/PlaylistPlay.js
@@ -5,12 +5,24 @@ class Playlist extends React.Component {
     constructor(props) {
         super(props)
         this.state = { playlist: _.cloneDeep(props.playlist),
-        playing_video:""}
+        playing_video_index:0}
+        this.play_next_vid=this.play_next_vid.bind(this);
     }
     componentDidMount(){
+       
+    }
+    play_next_vid(){
+        console.log("video ended");
+        let temp_index = this.state.playing_video_index;
+        temp_index+=1;
+        if(temp_index>=this.state.playlist.videos.length){
+            temp_index=0;
+        }
         this.setState({
-            playing_video:this.state.playlist.videos[0].name,
+            playing_video_index:temp_index,
         })
+        console.log("video ended");
+        
     }
     render() {
 
@@ -19,14 +31,14 @@ class Playlist extends React.Component {
                 <Button icon onClick={this.state.quitVideo}>
                     <Icon name="close" />
                 </Button>
-                <video controls>
-                    <source src={this.state.playlist.videos[0].url} />
+                <video controls onEnded={this.play_next_vid}>
+                    <source src={this.state.playlist.videos[this.state.playing_video_index].url} />
                 </video>
 
                 <Menu style={{ "overflowX": "scroll", "height": "100%" }}>
 
                     {this.state.playlist.videos.map((video) =>
-                        <Menu.Item active={this.state.playing_video===video.name}>
+                        <Menu.Item active={this.state.playlist.videos[this.state.playing_video_index].name===video.name} key={video.name}>
                             <div style={{ "display": "flex", "flexDirection": "column", "padding": ".00cm", "width": "100%",}}>
                                 <div>
                                     <Image src={video.thumbnail_url} style={{"height":"1cm"}}/>
