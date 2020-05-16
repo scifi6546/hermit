732127ee508937cbf672b15524c282faf3744c73
BuildTools <nickalexeev@protonmail.com>
2020-05-16 10:35:07 -0800

    slight refactor

--- gulkana/src/lib.rs
+++ gulkana/src/lib.rs
@@ -1,7 +1,6 @@
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::ErrorKind;
 
 use std::fmt;
 //rust lint does not see that rand is used so to kill error
@@ -13,168 +12,12 @@ mod opt_pair;
 use opt_pair::{new_optstruct_a, new_optstruct_b, OptStruct};
 mod errors;
 use errors::*;
+mod node;
+use node::*;
 /// # Gulkana
 /// Gulkana is a lightweight key based database for string files.
 /// The main struct is DataStructure
 
-#[derive(PartialEq, Eq, Clone, Serialize, Deserialize)]
-pub struct Link<Key: std::clone::Clone, TypeLabel: std::clone::Clone> {
-    type_label: TypeLabel,
-    children: Vec<Key>,
-}
-#[derive(PartialEq, Eq, Clone, Serialize, Deserialize)]
-pub struct Node<
-    Key: std::cmp::PartialEq + std::clone::Clone,
-    Item: std::clone::Clone,
-    LinkLabel: std::clone::Clone,
-> {
-    item: OptStruct<Link<Key, LinkLabel>, Item>,
-}
-impl<
-        KeyType: std::cmp::PartialEq + std::clone::Clone,
-        DataType: std::clone::Clone,
-        LinkLabel: std::clone::Clone,
-    > Node<KeyType, DataType, LinkLabel>
-{
-    pub fn get_item(&self) -> Result<&DataType, DBOperationError> {
-        let data = self.item.b();
-        if data.is_some() {
-            return Ok(data.unwrap());
-        } else {
-            return Err(DBOperationError::NodeNotData);
-        }
-    }
-    pub fn get_item_mut(&mut self) -> Result<&mut DataType, DBOperationError> {
-        let data = self.item.b_mut();
-        if data.is_some() {
-            return Ok(data.unwrap());
-        } else {
-            return Err(DBOperationError::NodeNotData);
-        }
-    }
-    pub fn get_link(&self) -> Result<&Link<KeyType, LinkLabel>, DBOperationError> {
-        let data = self.item.a();
-        if data.is_some() {
-            return Ok(data.unwrap());
-        } else {
-            return Err(DBOperationError::NodeNotLink);
-        }
-    }
-}
-fn new_node<
-    K: std::cmp::PartialEq + std::clone::Clone,
-    I: std::clone::Clone,
-    LinkLabel: std::clone::Clone,
->(
-    input: I,
-) -> Node<K, I, LinkLabel>
-where
-    K: std::clone::Clone,
-    I: std::clone::Clone,
-{
-    let foo = Node {
-        item: new_optstruct_b(input),
-    };
-    return foo;
-}
-fn new_node_link<
-    K: std::cmp::PartialEq + std::clone::Clone,
-    I: std::clone::Clone,
-    LinkLabel: std::clone::Clone,
->(
-    input: &std::vec::Vec<K>,
-    link_type: LinkLabel,
-) -> Node<K, I, LinkLabel>
-where
-    K: std::clone::Clone,
-    I: std::clone::Clone,
-{
-    let foo = Node {
-        item: new_optstruct_a(Link {
-            children: input.clone(),
-            type_label: link_type,
-        }),
-    };
-    return foo;
-}
-
-impl DBOperationError {
-    fn to_string(self) -> String {
-        match self {
-            Self::SerializeError => "failed seriailzing database".into(),
-            Self::FSError => "Failed to write".into(),
-            Self::KeyAllreadyPresent => "Key Allready Present".into(),
-            Self::KeyNotFound => "Key Not found".into(),
-            Self::NodeNotLink => "Node Not Link".into(),
-            Self::NodeNotData => "Node Not Data".into(),
-            Self::ParseError => "Parse Error".into(),
-            Self::FileNotFound => "File Not Found".into(),
-            Self::FilePermissionDenied => "File Permission Denied".into(),
-            Self::NetworkConnectionRefused => "Network COnnection Refused".into(),
-            Self::NetworkConnectionReset => "Network Connection Reset".into(),
-            Self::NetworkNotConnected => "Network Not Connected".into(),
-            Self::NetworkAddressInUse => "Network Address In Use".into(),
-            Self::NetworkAddrNotAvailable => "Network Address Not Availible".into(),
-            Self::BrokenPipe => "Broken Pipe".into(),
-            Self::FileAlreadyExists => "File ALready Exists".into(),
-            Self::WouldBlock => "Would Block".into(),
-            Self::InvalidInput => "Invalid Input".into(),
-            Self::InvalidData => "Invalid Data".into(),
-            Self::TimedOut => "Timed Out".into(),
-            Self::Interrupted => "Interrupted".into(),
-            Self::Other => "Other".into(),
-            Self::UnexpectedEof => "Unexpected End of File".into(),
-        }
-    }
-}
-impl Into<String> for DBOperationError {
-    fn into(self) -> String {
-        self.to_string()
-    }
-}
-
-impl fmt::Display for DBOperationError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.clone().to_string())
-    }
-}
-impl From<SerializeError> for DBOperationError {
-    fn from(error: SerializeError) -> Self {
-        match error {
-            _ => Self::SerializeError,
-        }
-    }
-}
-impl From<serde_json::error::Error> for DBOperationError {
-    fn from(error: serde_json::error::Error) -> Self {
-        match error {
-            _ => Self::ParseError,
-        }
-    }
-}
-impl From<std::io::Error> for DBOperationError {
-    fn from(error: std::io::Error) -> Self {
-        match error.kind() {
-            ErrorKind::NotFound => Self::FileNotFound,
-            ErrorKind::PermissionDenied => Self::FilePermissionDenied,
-            ErrorKind::ConnectionRefused => Self::NetworkConnectionRefused,
-            ErrorKind::ConnectionReset => Self::NetworkConnectionReset,
-            ErrorKind::NotConnected => Self::NetworkNotConnected,
-            ErrorKind::AddrInUse => Self::NetworkAddressInUse,
-            ErrorKind::AddrNotAvailable => Self::NetworkAddrNotAvailable,
-            ErrorKind::BrokenPipe => Self::BrokenPipe,
-            ErrorKind::AlreadyExists => Self::FileAlreadyExists,
-            ErrorKind::WouldBlock => Self::WouldBlock,
-            ErrorKind::InvalidInput => Self::InvalidInput,
-            ErrorKind::InvalidData => Self::InvalidData,
-            ErrorKind::TimedOut => Self::TimedOut,
-            ErrorKind::Interrupted => Self::Interrupted,
-            ErrorKind::Other => Self::Other,
-            ErrorKind::UnexpectedEof => Self::UnexpectedEof,
-            _ => Self::Other,
-        }
-    }
-}
 /// Struct usd to store data
 /// Inorder to allow new fields in input struct to be added
 /// make all fields Optional e.g.
---
+++ gulkana/src/node.rs
@@ -0,0 +1,83 @@
+use crate::errors::*;
+use crate::opt_pair::*;
+use serde::{Deserialize, Serialize};
+#[derive(PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Link<Key: std::clone::Clone, TypeLabel: std::clone::Clone> {
+    pub type_label: TypeLabel,
+    pub children: Vec<Key>,
+}
+#[derive(PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Node<
+    Key: std::cmp::PartialEq + std::clone::Clone,
+    Item: std::clone::Clone,
+    LinkLabel: std::clone::Clone,
+> {
+    pub item: OptStruct<Link<Key, LinkLabel>, Item>,
+}
+impl<
+        KeyType: std::cmp::PartialEq + std::clone::Clone,
+        DataType: std::clone::Clone,
+        LinkLabel: std::clone::Clone,
+    > Node<KeyType, DataType, LinkLabel>
+{
+    pub fn get_item(&self) -> Result<&DataType, DBOperationError> {
+        let data = self.item.b();
+        if data.is_some() {
+            return Ok(data.unwrap());
+        } else {
+            return Err(DBOperationError::NodeNotData);
+        }
+    }
+    pub fn get_item_mut(&mut self) -> Result<&mut DataType, DBOperationError> {
+        let data = self.item.b_mut();
+        if data.is_some() {
+            return Ok(data.unwrap());
+        } else {
+            return Err(DBOperationError::NodeNotData);
+        }
+    }
+    pub fn get_link(&self) -> Result<&Link<KeyType, LinkLabel>, DBOperationError> {
+        let data = self.item.a();
+        if data.is_some() {
+            return Ok(data.unwrap());
+        } else {
+            return Err(DBOperationError::NodeNotLink);
+        }
+    }
+}
+pub fn new_node<
+    K: std::cmp::PartialEq + std::clone::Clone,
+    I: std::clone::Clone,
+    LinkLabel: std::clone::Clone,
+>(
+    input: I,
+) -> Node<K, I, LinkLabel>
+where
+    K: std::clone::Clone,
+    I: std::clone::Clone,
+{
+    let foo = Node {
+        item: new_optstruct_b(input),
+    };
+    return foo;
+}
+pub fn new_node_link<
+    K: std::cmp::PartialEq + std::clone::Clone,
+    I: std::clone::Clone,
+    LinkLabel: std::clone::Clone,
+>(
+    input: &std::vec::Vec<K>,
+    link_type: LinkLabel,
+) -> Node<K, I, LinkLabel>
+where
+    K: std::clone::Clone,
+    I: std::clone::Clone,
+{
+    let foo = Node {
+        item: new_optstruct_a(Link {
+            children: input.clone(),
+            type_label: link_type,
+        }),
+    };
+    return foo;
+}
